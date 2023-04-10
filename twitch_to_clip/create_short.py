2e9dab383c4c7f5c2215bec10f9476a2e112deee
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2023-04-10 14:18:25 +0100

    removed black at the end

--- twitch_to_clip/create_short.py
+++ twitch_to_clip/create_short.py
@@ -6,16 +6,12 @@ from twitch_to_clip.ffmpeg_funcs import (
 )
 
 
-# * Example
-# createYtShort("raw_video.mp4", "video.mp4", [
-#     lambda name, out, file: audioFilter(name, out, file, "highpass=f=90")
-# ])
 def createShort(clipName: str, outputName: str, ffmpegArgs: str = None):
     createVerticalVideo(clipName, outputName)
 
-    videoLength = getVidLength(outputName)
-    if videoLength < 15:
-        addBlackToEnd(outputName, outputName, 15 - videoLength)
+    # videoLength = getVidLength(outputName)
+    # if videoLength < 15:
+    #     addBlackToEnd(outputName, outputName, 15 - videoLength)
 
     if ffmpegArgs:
         print("\n---applying custom ffmpeg arguments---\n")
